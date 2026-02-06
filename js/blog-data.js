398a3b8d1881434148fda9641514533f49f94920
GarvitRastogi978 <garvitrastogi834@gmail.com>
2026-02-06 13:49:08 +0530

    Added NEW BLOG

--- js/blog-data.js
+++ js/blog-data.js
@@ -1,14 +1,14 @@
 const blogs = [
   {
     title: "😏 Cyberpunk + Nano Banana style prompts",
-    date: "📅 Feb 1, 2026",
+    date: "📅 Feb 01, 2026",
     image: "assets/blog-cover-image/Cyberpunk + Nano Banana style prompts.jpg",
     file: "blogs/Cyberpunk + Nano Banana style prompts.html"
   },
   {
-    title: "Ultra Realistic AI Portrait Tricks",
-    date: "📅 May 5, 2026",
-    image: "assets/images/ai3.jpg",
-    file: "blogs/viral-ai-portrait-prompts.html"
+    title: "🌳 Cinematic Nature Wallpapers Prompts",
+    date: "📅 May 02, 2026",
+    image: "assets/blog-cover-image/cinematic-nature-image-wallpaper-prompts.jpg",
+    file: "blogs/Cinematic-nature-wallpapers.html"
   }
 ];
