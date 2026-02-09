d4dac2916e48fcaeb30a5d5d935d546989a69c54
GarvitRastogi978 <garvitrastogi834@gmail.com>
2026-02-09 13:06:43 +0530

    ADDED NEW PROMPT BLOG

--- js/blog-data.js
+++ js/blog-data.js
@@ -1,15 +1,21 @@
 const blogs = [
   {
+    title: "🍌 Nano Banana Caricature Funny Woman Prompts",
+    date: "📅 May 09, 2026",
+    image: "assets/blog-cover-image/caricature-funny-woman.png",
+    file: "blogs/Caricature-funny-woman-prompts.html"
+  },
+  {
     title: "🍌 Nano Banana Valentine's Day Couple Photo Prompts",
     date: "📅 May 08, 2026",
     image: "assets/blog-cover-image/Valentine's-Day-Couple-Photo Prompts.png",
     file: "blogs/Valentine's-Day-Couple-PhotoPrompts.html"
   },
   {
-    title: "🍌 Nano Banana Caricature Funny Woman Prompts",
-    date: "📅 May 09, 2026",
-    image: "assets/blog-cover-image/caricature-funny-woman.png",
-    file: "blogs/Caricature-funny-woman-prompts.html"
+    title: "🍌 Nano Banana Prompts: Funny and Cute Caricature",
+    date: "📅 May 08, 2026",
+    image: "assets/blog-cover-image/caricature-of-woman-with-friend.jpg",
+    file: "blogs/Create a Caricature-of-woman-with-friend.html"
   },
   {
     title: "🍌 Nano Banana Valentine's Day Beautiful Woman Portrait Prompts",
@@ -17,11 +23,11 @@ const blogs = [
     image: "assets/blog-cover-image/Valentine's-Day-Woman-Portrait-Prompts.png",
     file: "blogs/Valentine's-Day-Woman-Portrait-Prompts.html"
   },
-    {
-    title: "😏 Cyberpunk + Nano Banana style prompts",
-    date: "📅 Feb 01, 2026",
-    image: "assets/blog-cover-image/Cyberpunk + Nano Banana style prompts.jpg",
-    file: "blogs/Cyberpunk + Nano Banana style prompts.html"
+  {
+    title: "🍌 Nano Banana Split-Screen Woman Multiple Style Prompts",
+    date: "📅 May 04, 2026",
+    image: "assets/blog-cover-image/nano-banana-Split-Screen-Woman-Style-Prompts.png",
+    file: "blogs/Split-screen-Young-woman-prompts.html"
   },
   {
     title: "🌳 Cinematic Nature Wallpapers Prompts",
@@ -30,9 +36,9 @@ const blogs = [
     file: "blogs/Cinematic-nature-wallpapers.html"
   },
   {
-    title: "🍌 Nano Banana Split-Screen Woman Multiple Style Prompts",
-    date: "📅 May 04, 2026",
-    image: "assets/blog-cover-image/nano-banana-Split-Screen-Woman-Style-Prompts.png",
-    file: "blogs/Split-screen-Young-woman-prompts.html"
+    title: "😏 Cyberpunk + Nano Banana style prompts",
+    date: "📅 Feb 01, 2026",
+    image: "assets/blog-cover-image/Cyberpunk + Nano Banana style prompts.jpg",
+    file: "blogs/Cyberpunk + Nano Banana style prompts.html"
   }
 ];
