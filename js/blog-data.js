b2cac259d426d4fe0f6f558aa5597f5c83539e74
GarvitRastogi978 <garvitrastogi834@gmail.com>
2026-02-11 11:41:34 +0530

    ADDED NEW PROMPT BLOG

--- js/blog-data.js
+++ js/blog-data.js
@@ -1,49 +1,55 @@
 const blogs = [
   {
+    title: "Nano Banana Pro Prompt for Google Gemini AI: Realistic High-Resolution Women Biker Portrait",
+    date: "📅 Feb 11, 2026",
+    image: "assets/blog-cover-image/nano-banana-pro-woman-riding-bike-night.png",
+    file: "blogs/nano-banana-pro-woman-riding-bike-night.html"
+  },
+  {
     title: "🏁 Nano Banana Pro AI Woman at F1 Event — Hyper-Realistic Luxury Scene Created with Google Gemini",
-    date: "📅 May 10, 2026",
+    date: "📅 Feb 10, 2026",
     image: "assets/blog-cover-image/nano-banana-pro-ai-woman-f1-gemini.png",
     file: "blogs/nano-banana-pro-ai-woman-f1-gemini-prompts.html"
   },
   {
     title: "🍌 Gemini Nano Banana Pro Prompts for Beautiful Women Photos",
-    date: "📅 May 09, 2026",
+    date: "📅 Feb 09, 2026",
     image: "assets/blog-cover-image/Gemini-Nano-Banana-Prompt-Beautiful-Women.png",
     file: "blogs/Gemini Nano Banana Pro Prompts for Beautiful Women Photos.html"
   },
   {
     title: "🍌 Nano Banana Caricature Funny Woman Prompts",
-    date: "📅 May 09, 2026",
+    date: "📅 Feb 09, 2026",
     image: "assets/blog-cover-image/caricature-funny-woman.png",
     file: "blogs/Caricature-funny-woman-prompts.html"
   },
   {
     title: "🍌 Nano Banana Valentine's Day Couple Photo Prompts",
-    date: "📅 May 08, 2026",
+    date: "📅 Feb 08, 2026",
     image: "assets/blog-cover-image/Valentine's-Day-Couple-Photo Prompts.png",
     file: "blogs/Valentine's-Day-Couple-PhotoPrompts.html"
   },
   {
     title: "🍌 Nano Banana Prompts: Funny and Cute Caricature",
-    date: "📅 May 08, 2026",
+    date: "📅 Feb 08, 2026",
     image: "assets/blog-cover-image/caricature-of-woman-with-friend.jpg",
     file: "blogs/Create a Caricature-of-woman-with-friend.html"
   },
   {
     title: "🍌 Nano Banana Valentine's Day Beautiful Woman Portrait Prompts",
-    date: "📅 May 06, 2026",
+    date: "📅 Feb 06, 2026",
     image: "assets/blog-cover-image/Valentine's-Day-Woman-Portrait-Prompts.png",
     file: "blogs/Valentine's-Day-Woman-Portrait-Prompts.html"
   },
   {
     title: "🍌 Nano Banana Split-Screen Woman Multiple Style Prompts",
-    date: "📅 May 04, 2026",
+    date: "📅 Feb 04, 2026",
     image: "assets/blog-cover-image/nano-banana-Split-Screen-Woman-Style-Prompts.png",
     file: "blogs/Split-screen-Young-woman-prompts.html"
   },
   {
     title: "🌳 Cinematic Nature Wallpapers Prompts",
-    date: "📅 May 02, 2026",
+    date: "📅 Feb 02, 2026",
     image: "assets/blog-cover-image/cinematic-nature-image-wallpaper-prompts.jpg",
     file: "blogs/Cinematic-nature-wallpapers.html"
   },
